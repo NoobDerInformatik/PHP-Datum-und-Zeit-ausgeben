32fb75d02287c56726c791116dd9b0b72986edb3
Danny <70016750+NoobDerInformatik@users.noreply.github.com>
2020-08-22 21:05:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-  date_default_timezone_set("Europe/Berlin");
-  $time = date("G:i:s|d-m-Y");
-  echo $time;
+date_default_timezone_set("Europe/Berlin");
+$zeit = date("G:i:s");
+$datum = date("d-m-Y");
+echo "Zeit: ".$zeit."<br>Datum: ".$datum;
 ?>
